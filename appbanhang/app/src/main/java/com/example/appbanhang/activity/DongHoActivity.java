f78e5fef2db3f681074c0634233b8ed4d18aeeb9
ThanhPhu2 <phub1910124@student.ctu.edu.vn>
2023-09-05 22:25:13 +0700

    Thiet ke giao dien ban hang

---
+++ appbanhang/app/src/main/java/com/example/appbanhang/activity/DongHoActivity.java
@@ -0,0 +1,144 @@
+package com.example.appbanhang.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.appbanhang.R;
+import com.example.appbanhang.adapter.DongHoAdapter;
+import com.example.appbanhang.model.SanPhamMoi;
+import com.example.appbanhang.retrofit.ApiBanHang;
+import com.example.appbanhang.retrofit.RetrofitClient;
+import com.example.appbanhang.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class DongHoActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+    ApiBanHang apiBanHang;
+    CompositeDisposable compositeDisposable = new CompositeDisposable();
+    int page =1;
+    int loai;
+    DongHoAdapter adapterDt;
+    List<SanPhamMoi> sanPhamMoiList;
+    LinearLayoutManager linearLayoutManager;
+    Handler handler = new Handler();
+    boolean isLoading = false;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dien_thoai);
+        apiBanHang = RetrofitClient.getInstance(Utils.BASE_URL).create(ApiBanHang.class);
+        loai = getIntent().getIntExtra("loai",1);
+        AnhXa();
+        ActionToolBar();
+        getData(page);
+        addEventLoad();
+    }
+
+    private void addEventLoad() {
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (isLoading == false){
+                    if (linearLayoutManager.findLastCompletelyVisibleItemPosition() == sanPhamMoiList.size()-1){
+                        isLoading = true;
+                        loadMore();
+                    }
+                }
+            }
+        });
+    }
+
+    private void loadMore() {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                sanPhamMoiList.add(null);
+                adapterDt.notifyItemInserted(sanPhamMoiList.size()-1);
+            }
+        });
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                sanPhamMoiList.remove(sanPhamMoiList.size()-1);
+                adapterDt.notifyItemRemoved(sanPhamMoiList.size());
+                page = page+1;
+                getData(page);
+                adapterDt.notifyDataSetChanged();
+                isLoading = false;
+            }
+        },2000);
+    }
+
+    private void getData(int page) {
+        compositeDisposable.add(apiBanHang.getSanPham(page,loai)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        sanPhamMoiModel -> {
+                            if (sanPhamMoiModel.isSuccess()){
+                                    sanPhamMoiList = sanPhamMoiModel.getResult();
+                                    adapterDt = new DongHoAdapter(getApplicationContext(),sanPhamMoiList);
+                                    recyclerView.setAdapter(adapterDt);
+                            }
+                        },
+                        throwable -> {
+                            Toast.makeText(getApplicationContext(),"KHONG KET NOI DUOC VOI SERVER"+throwable.getMessage(),Toast.LENGTH_LONG).show();
+                        }
+                ));
+
+
+
+    }
+
+    private void ActionToolBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    private void AnhXa() {
+        toolbar = findViewById(R.id.toolbar);
+        recyclerView = findViewById(R.id.recyclerview_dt);
+        linearLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+        sanPhamMoiList = new ArrayList<>();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        compositeDisposable.clear();
+        super.onDestroy();
+    }
+}
---
+++ appbanhang/app/src/main/java/com/example/appbanhang/utils/Utils.java
@@ -0,0 +1,10 @@
+package com.example.appbanhang.utils;
+
+import com.example.appbanhang.model.GioHang;
+
+import java.util.List;
+
+public class Utils {
+    public static final String BASE_URL="http://10.2.9.36/banhang/";
+    public  static List<GioHang> manggiohang;
+}
